d89157b131f4c825d5db779307904eb767fa20bc
Victor Gabriel <victorgabdev@gmail.com>
2024-05-06 10:12:09 -0300

    updated names and assignment methods of AddressService

--- src/main/java/com/victorgabdev/gerenciapessoas/core/address/AddressService.java
+++ src/main/java/com/victorgabdev/gerenciapessoas/core/address/AddressService.java
@@ -1,10 +1,14 @@
 package com.victorgabdev.gerenciapessoas.core.address;
 
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface AddressService {
-    Optional<Address> getAddressById(Long id);
-    Address createAddress(Address address);
-    Address updateAddress(Long id, Address updatedAddress);
-    void deleteAddress(Long id);
+    Optional<Address> getAddressToPerson(Long personId);
+    List<Address> getAllAddressToPerson(Long personId);
+    Address createAddressToPerson(Long personId, Address address);
+    Address updateAddressToPerson(Long personId ,Long addressId, Address updatedAddress);
+    void deleteAddressToPerson(Long personId ,Long addressId);
 }
