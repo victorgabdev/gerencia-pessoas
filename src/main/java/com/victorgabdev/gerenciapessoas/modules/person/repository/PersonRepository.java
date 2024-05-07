9ae33aa33e9d6d16cb6bb27914b1a8af0f457d91
Victor Gabriel <victorgabdev@gmail.com>
2024-05-07 11:44:22 -0300

    created method findByFullName

--- src/main/java/com/victorgabdev/gerenciapessoas/modules/person/repository/PersonRepository.java
+++ src/main/java/com/victorgabdev/gerenciapessoas/modules/person/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.victorgabdev.gerenciapessoas.modules.person.repository;
 import com.victorgabdev.gerenciapessoas.core.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByFullName(String fullName);
 }
