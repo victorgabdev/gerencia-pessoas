502b48dc0f66bd273e10ddb4e4a34e26e59b3ce1
Victor Gabriel <victorgabdev@gmail.com>
2024-05-07 10:50:56 -0300

    moved repositories to module layer

--- src/main/java/com/victorgabdev/gerenciapessoas/modules/address/repository/AddressRepository.java
+++ src/main/java/com/victorgabdev/gerenciapessoas/modules/address/repository/AddressRepository.java
@@ -1,4 +1,5 @@
-package com.victorgabdev.gerenciapessoas.core.address;
+package com.victorgabdev.gerenciapessoas.modules.address.repository;
+import com.victorgabdev.gerenciapessoas.core.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
--- src/main/java/com/victorgabdev/gerenciapessoas/modules/person/repository/PersonRepository.java
+++ src/main/java/com/victorgabdev/gerenciapessoas/modules/person/repository/PersonRepository.java
@@ -1,5 +1,6 @@
-package com.victorgabdev.gerenciapessoas.core.person;
+package com.victorgabdev.gerenciapessoas.modules.person.repository;
 
+import com.victorgabdev.gerenciapessoas.core.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
