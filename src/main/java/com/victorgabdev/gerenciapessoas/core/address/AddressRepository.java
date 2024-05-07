af96e0543ddb4f1b2abaa82fce7499008e673f48
Victor Gabriel <victorgabdev@gmail.com>
2024-05-07 10:07:05 -0300

    added method findByIdAndPersonId to class AddressRepository

--- src/main/java/com/victorgabdev/gerenciapessoas/core/address/AddressRepository.java
+++ src/main/java/com/victorgabdev/gerenciapessoas/core/address/AddressRepository.java
@@ -1,6 +1,9 @@
 package com.victorgabdev.gerenciapessoas.core.address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findByIdAndPersonId(Long id, Long personId);
 }
